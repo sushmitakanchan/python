b1132a0b8e97c0a1c0fd115a28537c1e5440d6d7
sushmitakanchan <31158191+sushmitakanchan@users.noreply.github.com>
2020-07-23 21:53:13 +0530

    Assignment 2

---
+++ Python assignment 2.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+"""Helloworld"""
+
+
+# In[4]:
+
+
+print("hello        world")
+
+
+# In[5]:
+
+
+print("python's world")
+
+
+# # Spaces
+
+# In[6]:
+
+
+print("python\tworld")
+
+
+# 
+# # New Line
+
+# In[7]:
+
+
+print("Hello\nworld")
+
+
+# # Formatted output
+
+# In[8]:
+
+
+name="Sush"
+age=21
+marks=87.86
+print(f"The name of person is {name} age is {age} the marks is {marks}")
+
+
+# In[9]:
+
+
+print("The name of person is %10s marks is %10.2f age is %10d"%(name,marks,age))
+
+
+# In[10]:
+
+
+5**5
+
+
+# In[11]:
+
+
+10+20
+
+
+# In[12]:
+
+
+10-20
+
+
+# In[13]:
+
+
+10/2
+
+
+# In[14]:
+
+
+10/3
+
+
+# In[15]:
+
+
+10%3
+
+
+# In[16]:
+
+
+10//3
+
+
+# In[17]:
+
+
+a=10
+b=20
+a==b
+
+
+# In[18]:
+
+
+c=10
+a==c
+
+
+# In[19]:
+
+
+c<=a
+
+
+# In[20]:
+
+
+a>=b
+
+
+# In[21]:
+
+
+a!=b
+
+
+# In[22]:
+
+
+str1="pythonprogramming"
+"x" in str1
+
+
+# In[23]:
+
+
+a=10
+b=10
+a==b
+
+
+# In[24]:
+
+
+a is b
+
+
+# In[25]:
+
+
+id(a)
+
+
+# In[26]:
+
+
+id(b)
+
+
+# In[27]:
+
+
+x=1.5
+y=1.5
+x==y
+
+
+# In[28]:
+
+
+x is y
+
+
+# In[29]:
+
+
+id(x)
+
+
+# In[30]:
+
+
+id(y)
+
+
+# In[31]:
+
+
+a=257
+b=257
+a==b
+
+
+# In[32]:
+
+
+a is b
+
+
+# In[33]:
+
+
+10>8
+
+
+# In[34]:
+
+
+8>9
+
+
+# In[ ]:
+
+
+
+
